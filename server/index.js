ceb3a5fd7bcebec43f9d515a3f37b8f29ddbc646
Jonathon Flores <jonathon@Jonathons-MacBook-Pro.local>
2021-07-14 17:50:49 -0500

    configured node to serve up the build folder for frontend

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3002;
@@ -6,7 +7,13 @@ const db = require("./db");
 
 const product = require("./controllers/product");
 
+app.use(express.static(`${__dirname}/../build`));
+
 app.get("/api/products", product.getProducts);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
